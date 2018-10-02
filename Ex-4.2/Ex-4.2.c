364c61d69f9837b138b40064846842f3459421b6
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-10-02 11:55:36 -0300

    Update Ex-4.2.c

--- Ex-4.2/Ex-4.2.c
+++ Ex-4.2/Ex-4.2.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 
-int todos_os_primos(int max){
+int eh_primo(int n){
     int i,qtd=0;
-    while (max>0){
-         for(i=1;i<=max;i++){
-              if(max%i==0){
-                   qtd+=1;
-               }
-         }if(qtd==2){
-              printf("%d \n",max);
-         }
-         max--;
-         qtd=0;
+    for(i=1;i<=n;i++){
+        if(n%i==0){
+            qtd++;
+        }
+    }
+    if(qtd==2){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+void todos_os_primos(int n){
+    printf(":::::NUMEROS:::::\n");
+    while(n>0){
+        int x=eh_primo(n);
+        if(x==1){
+            printf("%d\n",n);
+        }
+        n--;
     }
 }
 int main(void){
     int max;
     printf("Digite um numero: ");
     scanf("%d",&max);
-    int x=todos_os_primos(max);
+    todos_os_primos(max);
     return 0;
 }
