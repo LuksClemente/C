51cfdd02e5647c127d766ea7abc9e12df094fcf0
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-10-19 17:42:37 -0300

    Update Exercicio-2.2.c

--- Ex-2.2/Exercicio-2.2.c
+++ Ex-2.2/Exercicio-2.2.c
@@ -7,10 +7,18 @@ int main (void){
 	scanf("%d", &num1);
 	printf("Escolha outro numero: ");
 	scanf("%d", &num2);
-	int i=num1;
-	while (i<(num2+1)){
-		printf("%d\n", i);
-		i=i+1;
+	if(num2>num1){
+		while (num1<(num2+1)){
+			printf("%d\n", num1);
+			num1=num1+1;
+		}
+	}else if(num1>num2){
+		while (num2<(num1+1)){
+			printf("%d\n",num2);
+			num2=num2+1;
+		}
+	}else{
+		printf("Valores iguais!");
 	}
 	return 0;
 }
