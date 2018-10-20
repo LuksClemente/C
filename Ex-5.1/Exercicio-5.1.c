0930f8fc98f05736e4f70cdfccdb79423b7ae6ea
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-10-20 12:21:32 -0300

    Update Exercicio-5.1.c

--- Ex-5.1/Exercicio-5.1.c
+++ Ex-5.1/Exercicio-5.1.c
@@ -2,23 +2,27 @@
 #include <stdlib.h>
 
 int main(void){
-    int a,b;
-    int* p;
-    printf("Digite um numero: ");
-    scanf("%d",&a);
-    printf("Digite outro numero: ");
-    scanf("%d",&b);
-    printf(":::::VALORES ORIGINAIS:::::\n");
-    printf("Valor de [a]: %d\nValor de [b]: %d\n",a,b);
-    printf(":::::VALORES ALTERADOS:::::\n");
-    if(a>b){
-        int* p=&a;
-        *p=a-50;
-        printf("Valor de [a]: %d\nValor de [b]: %d\n",*p,b);
-    }else{
-        int* p=&b;
-        *p=b-50;
-        printf("Valor de [a]: %d\nValor de [b]: %d\n",a,*p);
-    }
-    return 0;
+	int a,b;
+	int* p;
+	printf("Digite um valor: ");
+	scanf("%d",&a);
+	printf("Digite outro valor: ");
+	scanf("%d",&b);
+	printf(":::::VALORES ORIGINAIS:::::\n");
+	printf("[a]=%d\n[b]=%d\n",a,b);
+	if(a>b){
+		p=&a;
+		*p=a-50;
+		printf(":::::VALORES ALTERADOS:::::\n");
+		printf("[a]=%d\n[b]=%d\n",*p,b);
+	}else if(a<b){
+		p=&b;
+		*p=b-50;
+		printf(":::::VALORES ALTERADOS:::::\n");
+		printf("[a]=%d\n[b]=%d\n",a,*p);
+	}else{
+		printf(":::::VALORES ALTERADOS:::::\n");
+		printf("Valores iguais, alteracao nao possivel");
+	}
+	return 0;
 }
