500cf390a4ccd2e2d5445769a0de863d6377b741
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-10-20 12:56:52 -0300

    Update Exercicio-5.3.c

--- Ex-5.3/Exercicio-5.3.c
+++ Ex-5.3/Exercicio-5.3.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 void compra(int* conta,int valor){
-	*conta=*conta-valor;
-	printf(":::::SALDO ATUALIZADO:::::\nApós a compra: R$ %d,00\n",*conta);
+	*conta-=valor;
 }
 int main(void){
 	int compras[]={100,50,80,30,20};
-	int minha_conta1,minha_conta2;
-	int valor,i;
+	int minha_conta1,minha_conta2,i,valor;
 	int* conta;
-	printf("Digite o valor da Conta 1: ");
-    scanf("%d",&minha_conta1);
-    printf("Digite o valor da Conta 2: ");
-    scanf("%d",&minha_conta2);
-    for(i=0;i<5;i++){
-    	valor=compras[i];
-    	if(minha_conta1>minha_conta2){
-    		conta=&minha_conta1;
-    		compra(conta,valor);
+	printf(":::::INSERIR DADOS:::::\n");
+	printf("Digite o valor da conta[1]: ");
+	scanf("%d",&minha_conta1);
+	printf("Digite o valor da conta[2]: ");
+	scanf("%d",&minha_conta2);
+	for(i=0;i<5;i++){
+		valor=compras[i];
+		if(minha_conta1>minha_conta2){
+			conta=&minha_conta1;
+			compra(conta,valor);
 		}else{
 			conta=&minha_conta2;
 			compra(conta,valor);
 		}
+		printf("::::::SALDO FINAL::::::\n");
+		printf("Saldo Conta[1]: R$%d,00\nSaldo Conta[2]: R$%d,00\n",minha_conta1,minha_conta2);
 	}
-	printf(":::::SALDO FINAL:::::\n");
-    printf("Conta 1: R$ %d,00\nConta 2: R$ %d,00\n",minha_conta1,minha_conta2);
-    return 0;
+	return 0;
 }
