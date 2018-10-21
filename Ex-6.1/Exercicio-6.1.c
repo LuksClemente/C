f19d6e0bd7d2ac9d66a050a9f5a9ba82b316ea85
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-10-21 12:16:32 -0300

    Update Exercicio-6.1.c

--- Ex-6.1/Exercicio-6.1.c
+++ Ex-6.1/Exercicio-6.1.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void preenche(int* vetor,int n){
+void preenche(int* vec,int n){
 	int i;
 	for(i=0;i<n;i++){
-		printf("Digite um valor: ");
-		scanf("%d",vetor+i);
+		printf("Digite o valor[%d]: ",i+1);
+		scanf("%d",vec+i);
 	}
 }
-void media(int* vetor,int n){
+float media(int* vec,int n){
 	int i;
-	float soma,medium;
+	float soma=0,med;
 	for(i=0;i<n;i++){
-		soma+=*(vetor+i);
+		soma+=*(vec+i);
 	}
-	medium=(soma/n);
-	printf(":::::::::::::::::::::::\n");
-	printf("MEDIA DOS VALORES: %f\n",medium);
-	printf(":::::::::::::::::::::::");
+	med=(soma/n);
+	return med;
 }
 int main(void){
-	int n,vetor[n];
-	printf("Digite o numero de valores: ");
+	int n,vec[100];
+	printf(":::::INSERIR DADOS:::::\n");
+	printf("Digite o tamanho do vetor: ");
 	scanf("%d",&n);
-	preenche(vetor,n);
-	media(vetor,n);
-	return 0;	
+	preenche(vec,n);
+	float x=media(vec,n);
+	printf(":::::::::MEDIA:::::::::\n");
+	printf("Valor da media: %.2f\n",x);
+	printf(":::::::::::::::::::::::\n");
+	return 0;
 }
