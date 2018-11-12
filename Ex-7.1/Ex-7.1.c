34f9e850eebb3ba556d5d19485bce99011224c37
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-11-12 10:06:17 -0200

    Update Ex-7.1.c

--- Ex-7.1/Ex-7.1.c
+++ Ex-7.1/Ex-7.1.c
@@ -10,13 +10,13 @@ void preenche(struct Campeao* c){
     int* v,m,d,h;
     printf("Digite os atributos de seu campeao: ");
     scanf("%d %d %d %d",&((*c).vida),&((*c).mana),&((*c).dano),&((*c).habilidade));
-    printf("VIDA: %d\n",(*c).vida);
-    printf("MANA: %d\n",(*c).mana);
-    printf("DANO: %d\n",(*c).dano);
-    printf("HABILIDADE: %d\n",(*c).habilidade);
-}
+}   
 int main(void){
     struct Campeao champ;
     preenche(&champ);
+    printf("VIDA: %d\n",champ.vida);
+    printf("MANA: %d\n",champ.mana);
+    printf("DANO: %d\n",champ.dano);
+    printf("HABILIDADE: %d\n",champ.habilidade);
     return 0;
 }
