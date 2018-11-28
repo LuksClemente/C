aa6e06f47314cb0a0f9a34c366f72037d0bd14e4
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-11-28 09:34:49 -0200

    Create Ex-8.1.c

---
+++ Ex-8.1/Ex-8.1.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char s[25];
+    int x,i;
+    scanf("%s",s);
+    x=strlen(s);
+    FILE* f=fopen("/tmp/arq.txt","w");
+    for(i=0;i<x;i++){
+        fputc(s[i],f);    
+    }
+    fclose(f);
+    return 0;
+}
