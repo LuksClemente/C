0c3637b2bb08467182c4dbba2a20415b917ae369
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-11-28 10:28:24 -0200

    Create Ex-8.2.c

---
+++ Ex-8.2/Ex-8.2.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    int i,qtd=0;
+    char ch;    
+    FILE* f=fopen("/tmp/arq.txt","r");
+    while((ch=fgetc(f)) != EOF){
+        fprintf(f,"%c",ch);
+        fprintf(stdout,"%c",ch);
+    }
+    printf("\n");
+    fclose(f);
+    return 0;
+}
