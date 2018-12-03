5626c54b6ba4de8ccfdfd970444da467c72ac5fd
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-12-03 09:09:37 -0200

    Update Ex-8.2.c

--- Ex-8.2/Ex-8.2.c
+++ Ex-8.2/Ex-8.2.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
 int main(void){
-    int i,qtd=0;
-    char ch;    
-    FILE* f=fopen("/tmp/arq.txt","r");
-    while((ch=fgetc(f)) != EOF){
-        fprintf(f,"%c",ch);
-        fprintf(stdout,"%c",ch);
-    }
-    printf("\n");
-    fclose(f);
-    return 0;
+	int i=0;
+	char string[25];
+	FILE* f=fopen("/home/ime/arq.txt","r");	
+	string[i]=fgetc(f);	
+	while(string[i] != EOF){
+		i++;	
+		string[i]=fgetc(f);	
+	}
+	string[i]='\0';
+	fclose(f);
+	printf("%s\n",string);
+	return 0;
 }
