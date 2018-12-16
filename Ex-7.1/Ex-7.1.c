f6fecf9e823cdb3011375242bf2e1bc20714d7b2
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-12-16 14:43:23 -0200

    Update Ex-7.1.c

--- Ex-7.1/Ex-7.1.c
+++ Ex-7.1/Ex-7.1.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Cinema{
-	int sala;
-	int ingressos;
+struct Jogador{
+	int passe;
+	int drible;
 };
-void preenche(struct Cinema* cine){
-	printf("Qual a sala do filme?: ");
-	scanf("%d",&(*cine).sala);
-	printf("Quantos ingressos?: ");
-	scanf("%d",&(*cine).ingressos);
+void preenche(struct Jogador* p1){
+	printf("Quantos de passe[0-100]?: ");
+	scanf("%d",&(*p1).passe);
+	printf("Quanto de drible[0-100]?: ");
+	scanf("%d",&(*p1).drible);
 }
 int main(void){
-	struct Cinema cinema;
-	preenche(&cinema);
-	printf("SALA: %d\n",cinema.sala);
-	printf("QTD INGRESSOS: %d\n",cinema.ingressos);
+	struct Jogador player;
+	preenche(&player);
+	printf("PASSE: %d\n",player.passe);
+	printf("DRIBLE: %d\n",player.drible);
 	return 0;
 }
