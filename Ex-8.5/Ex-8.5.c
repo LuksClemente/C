faf51b811946b579d20cb34e6eda7c5595d188bb
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-12-18 18:04:56 -0200

    Update Ex-8.5.c

--- Ex-8.5/Ex-8.5.c
+++ Ex-8.5/Ex-8.5.c
@@ -1,76 +1,68 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-struct Campeao{
-	int vida;
-	int mana;	
+struct Jogador{
+	int passe;
+	int drible;
 	union{
-		int dano;
-    		int habilidade;
-	}reg;
+		int finalizacao;
+		int defesa;
+	}posicao;
 };
-struct Campeao2{
-    	char nome[50];
-    	struct Campeao c;
+struct Player{
+	char nome[20];
+	struct Jogador p;
 };
-void preenche(struct Campeao* c1,int esc){	
-    	printf("DIGITE A VIDA DE SEU CAMPEÃO: ");
-    	scanf("%d",&((*c1).vida));
-    	printf("DIGITE A MANA DE SEU CAMPEÃO: ");
-    	scanf("%d",&((*c1).mana));
-    	if(esc==1){
-    		printf("DIGITE O DANO DO SEU CAMPEÃO: ");
-    		scanf("%d",&((*c1).reg.dano));
-	}if(esc==2){
-		printf("DIGITE A HABILIDADE DO SEU CAMPEÃO: ");
-		scanf("%d",&((*c1).reg.habilidade));
+void preenche(struct Jogador* p1,int esc){
+	printf("Quantos de passe[0-100]?: ");
+	scanf("%d",&(*p1).passe);
+	printf("Quanto de drible[0-100]?: ");
+	scanf("%d",&(*p1).drible);
+	if(esc==1){
+		printf("Quanto de finalizacao[0-100]: ");
+		scanf("%d",&(*p1).posicao.finalizacao);
+	}else{
+		printf("Quanto de defesa[0-100]: ");
+		scanf("%d",&(*p1).posicao.defesa);
 	}
 }
-void preenche2(struct Campeao2* c2,int e){
-    	printf("DIGITE O NOME DO SEU CAMPEAO: ");
-    	scanf("%s",((*c2).nome));
-    	preenche((&(*c2).c),e);
-} 
+void preenche2(struct Player* p1,int e){
+	printf("Nome do jogador: ");
+	scanf("%s",(*p1).nome);
+	preenche(&(*p1).p,e);
+}
 int main(void){
 	int escolha;
-	printf("ESCOLHA ENTRE DANO OU HABILIDADE[1/2]: ");
+	printf("Jogador de linha ou goleiro?[1/2]: ");
     	scanf("%d",&escolha);
-    	struct Campeao2 c2;
-    	preenche2(&c2,escolha);
-    	FILE* f=fopen("/home/ime/campeaolol.txt","wb");
-    	if (escolha==1){
-        	fwrite(&c2.nome,sizeof(char),(strlen(c2.nome)+1),f);
-    		fwrite(&c2.c.vida,sizeof(int),1,f);
-    		fwrite(&c2.c.mana,sizeof(int),1,f);
-    		fwrite(&c2.c.reg.dano,sizeof(int),1,f);
+    	struct Player player;
+    	preenche2(&player,escolha);
+    	FILE* f=fopen("E:/Documentos/Lucas Clemente/Listas CC UERJ/Linguagem de Programação I/arq.txt","wb");
+    	fwrite(&player.nome,sizeof(char),(strlen(player.nome)+1),f);
+    	fwrite(&player.p.passe,sizeof(int),1,f);
+    	fwrite(&player.p.drible,sizeof(int),1,f);
+		if (escolha==1){
+    		fwrite(&player.p.posicao.finalizacao,sizeof(int),1,f);
     	}if(escolha==2){
-        	fwrite(&c2.nome,sizeof(char),(strlen(c2.nome)+1),f);
-    		fwrite(&c2.c.vida,sizeof(int),1,f);
-    		fwrite(&c2.c.mana,sizeof(int),1,f);
-    		fwrite(&c2.c.reg.habilidade,sizeof(int),1,f);
+    		fwrite(&player.p.posicao.defesa,sizeof(int),1,f);
 	}
     	fclose(f);
-    	FILE* g=fopen("/home/ime/campeaolol.txt","rb");
-    	struct Campeao2 c3;
-    	if(escolha==1){
-        	fread(&c3.nome,sizeof(char),(strlen(c2.nome)+1),g);
-        	fread(&c3.c.vida,sizeof(int),1,g);
-        	fread(&c3.c.mana,sizeof(int),1,g);
-        	fread(&c3.c.reg.dano,sizeof(int),1,g);
-        	printf("NOME: %s\n",c3.nome);
-    		printf("VIDA: %d\n",c3.c.vida);
-    		printf("MANA: %d\n",c3.c.mana);
-        	printf("DANO: %d\n",c3.c.reg.dano);
+    	FILE* g=fopen("E:/Documentos/Lucas Clemente/Listas CC UERJ/Linguagem de Programação I/arq.txt","rb");
+    	struct Player player2;
+    	fread(&player2.nome,sizeof(char),(strlen(player.nome)+1),g);
+        fread(&player2.p.passe,sizeof(int),1,g);
+        fread(&player2.p.drible,sizeof(int),1,g);
+        printf("NOME: %s\n",player2.nome);
+        printf("PASSE: %d\n",player2.p.passe);
+        printf("DRIBLE: %d\n",player2.p.drible);
+		if(escolha==1){
+        	fread(&player2.p.posicao.finalizacao,sizeof(int),1,g);
+        	printf("FINALIZACAO: %d\n",player2.p.posicao.finalizacao);
     	}
     	if(escolha==2){
-        	fread(&c3.nome,sizeof(char),(strlen(c2.nome)+1),g);
-        	fread(&c3.c.vida,sizeof(int),1,g);
-        	fread(&c3.c.mana,sizeof(int),1,g);
-        	fread(&c3.c.reg.habilidade,sizeof(int),1,g);
-        	printf("NOME: %s\n",c3.nome);
-    		printf("VIDA: %d\n",c3.c.vida);
-    		printf("MANA: %d\n",c3.c.mana);
-        	printf("HABILIDADE: %d\n",c3.c.reg.habilidade);
+        	fread(&player2.p.posicao.defesa,sizeof(int),1,g);
+        	printf("DEFESA: %d\n",player2.p.posicao.defesa);
     	}
     	return 0;
 }
