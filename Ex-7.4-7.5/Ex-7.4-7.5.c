38eccff08514885d9ff3069eb948637f77278885
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-12-18 19:37:41 -0200

    Update Ex-7.4-7.5.c

--- Ex-7.4-7.5/Ex-7.4-7.5.c
+++ Ex-7.4-7.5/Ex-7.4-7.5.c
@@ -1,59 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct Pos{
+struct Posicao{
 	int x;
 	int y;
 };
-struct Pers{
+struct Personagem{
 	int id;
-	struct Pos p;
+	struct Posicao s;
 	int score;
 };
-void posicao(struct Pos* p){
-	printf("Digite a coordenadas X[0-9]: ");
-	scanf("%d",&(*p).x);
-	printf("Digite a coordenadas Y[0-9]: ");
-	scanf("%d",&(*p).y);
+void preenche(struct Posicao* s){
+	printf("Digite x: ");
+	scanf("%i",&s->x);
+	printf("Digite y: ");
+	scanf("%i",&s->y);
 }
-void personagem(struct Pers* p1){
-	printf("Digite o ID do personagem: ");
-	scanf("%d",&(*p1).id);
-	(*p1).score=0;
-	posicao(&(*p1).p);
-}
-void Mapa(char map[10][10]){
-	int i,j;
-	char m[]=" ";
-	map[0][0]=m[0];
-	for(i=0;i<10;i++){
-		map[i+1][0]=i;
-		map[0][i+1]=i;
-		for(j=0;j<10;j++){
-			map[i+1][j+1]=m[0];
-		}
-	}
+void preenche2(struct Personagem* p){
+	int i;
+	printf("Numero do participante: ");
+	scanf("%i", &p->id);
+	preenche(&p->s);
+	p->score=0;
 }
 int main(void){
-	struct Pers per[10];
-	int i,j;
-	for(i=0;i<10;i++){
-		personagem(&per[i]);
-	}
-	char mapa[10][10];
-	Mapa(mapa);
-	for(i=0;i<10;i++){
-		mapa[per[i].p.x+1][per[i].p.y+1]=per[i].id;
-	}
-	for(i=0;i<10;i++){
-		for(j=0;j<10;j++){
-			if(mapa[i][j]==32){
-				printf("%c ",mapa[i][j]);
-			}else{
-				printf("%d ",mapa[i][j]);
-			}
-		}
-		printf("\n");
+	struct Personagem per[10];
+	int i;
+	for (i=0;i<10;i+=1){
+		preenche2(&per[i]);
 	}
 	return 0;
 }
