2ddfcacb3b111079d4ce018699db2feaa7df86e9
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2018-12-18 19:47:15 -0200

    Update Ex-7.4-7.5.c

--- Ex-7.4-7.5/Ex-7.4-7.5.c
+++ Ex-7.4-7.5/Ex-7.4-7.5.c
@@ -22,11 +22,31 @@ void preenche2(struct Personagem* p){
 	preenche(&p->s);
 	p->score=0;
 }
+void mapa(struct Personagem* p, int n){
+	int y=0,x=0,e=0;
+	printf(" 0123456789\n");
+	for(y=0;y<10;y++){
+		printf("%d",y);
+		for(x=0;x<10;x++){
+			for(e=0;e<n;e++){
+				if((p+e)->s.y==y && (p+e)->s.x==x){
+					printf("%d",(p+e)->id);
+					break;
+				}
+				else if(e==(n-1)){
+					printf(" ");
+				}
+			}
+		}
+		printf("\n");
+	}
+}
 int main(void){
 	struct Personagem per[10];
 	int i;
-	for (i=0;i<10;i+=1){
+	for (i=0;i<10;i++){
 		preenche2(&per[i]);
 	}
+	mapa(per,10);
 	return 0;
 }
